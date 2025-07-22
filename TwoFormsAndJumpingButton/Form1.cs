7ca62058f5da00a12b7159600c7c75ba0100bcb0
Alex <aleksandrpermakov105@gmail.com>
2025-07-22 15:04:59 +0500

    Добавьте файлы проекта.

---
+++ TwoFormsAndJumpingButton/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoFormsAndJumpingButton
+{
+    public partial class Form1 : Form
+    {
+        Form form2;
+        Form form1;
+        public Form1()
+        {
+            InitializeComponent();
+            form1 = this;
+            Form2 frm = new Form2(form1);
+            form2 = frm;
+            frm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (form1.Controls.Contains(button1))
+            {
+                form2.Controls.Add(button1);
+            }
+            else
+            {
+                form1.Controls.Add(button1);
+            }
+            
+        }
+    }
+}
---
+++ TwoFormsAndJumpingButton/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoFormsAndJumpingButton
+{
+    public partial class Form2 : Form
+    {
+        Form form2;
+        Form form1;
+        public Form2(Form frm)
+        {
+            InitializeComponent();
+            form1 = frm;
+            form2 = this;
+        }
+        //if (form1.Controls.Contains(button1))
+        //    {
+        //        form2.Controls.Add(button1);
+        //    }
+        //    else
+        //    {
+        //        form1.Controls.Add(button1);
+        //    }
+private void button2_Click(object sender, EventArgs e)
+        {
+            if (form1.Controls.Contains(textBox1))
+            {
+                form2.Controls.Add(textBox1);
+            }
+            else
+            {
+                form1.Controls.Add(textBox1);
+            }
+
+        }
+    }
+}
